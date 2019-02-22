46fdb65ed7e89ab19fa5491d71df966c925a9527
Joe Erato <8867587+joeerato@users.noreply.github.com>
2019-02-22 07:03:56 -0600

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,4 +3,4 @@
 import os
 
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
-SLACK_URL = os.getenv('https://hooks.slack.com/services/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')
+SLACK_URL = os.getenv('')
